33cfc9199630aa5ac82d943a50fff44e9e230648
David Ikin <davidikin45@gmail.com>
2020-04-02 14:54:33 +0100

    changes

--- AspNetCore.Mvc.UrlLocalization/UrlLocalizationRouteBuilderExtemsions.cs
+++ AspNetCore.Mvc.UrlLocalization/UrlLocalizationRouteBuilderExtemsions.cs
@@ -41,8 +41,8 @@ namespace AspNetCore.Mvc.UrlLocalization
             {
                 context.Response.StatusCode = StatusCodes.Status404NotFound;
                 return Task.CompletedTask;
-            });            
-            conventionBuilder.Add(b => ((RouteEndpointBuilder)b).Order = -1);
+            });
+            conventionBuilder.Add(b => ((RouteEndpointBuilder)b).Order = int.MaxValue);
 
             //redirect culture-less routes
             conventionBuilder = routes.MapGet("{**path}", (RequestDelegate)(ctx =>
@@ -55,7 +55,7 @@ namespace AspNetCore.Mvc.UrlLocalization
                 ctx.Response.Redirect(culturedPath);
                 return Task.CompletedTask;
             }));
-            conventionBuilder.Add(b => ((RouteEndpointBuilder)b).Order = -1);
+            conventionBuilder.Add(b => ((RouteEndpointBuilder)b).Order = int.MaxValue);
 
             return routes;
         }
